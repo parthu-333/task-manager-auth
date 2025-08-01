fea6796287fbca66454dfdf8f9df4617caebc8e8
parthu-333 <parthuchary333@gmail.com>
2025-08-01 19:36:11 +0530

    DataBase Changed

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,11 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/todos', todoRoutes)
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
 .then(() => {
-    app.listen(5000, () => console.log("Server is running at port 5000"));
+    app.listen(process.env.PORT || 10000, () => console.log("Server is running at port 5000"));
 })
 .catch(err => console.log(err));
